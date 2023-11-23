05818b661e0b27ce58438cef6918475475211c9b
Dang Nguyen <ven-dang.nguy@corsair.com>
2023-11-23 14:36:30 +0700

    Add Use3rdparty to Project

---
+++ src/Use3rdparty/include/Feature3rdparty.h
@@ -0,0 +1,10 @@
+#ifndef FEATURE3RDPARTY_H
+#define FEATURE3RDPARTY_H
+
+namespace Feature3rdparty
+{
+int myMultiplication(int a, int b);
+double mydivision(double a, double b);
+}
+
+#endif //FEATURE3RDPARTY_H
